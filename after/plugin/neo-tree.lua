375b46c8c147e7ae96819afbb7e94d36b1baa747
Dalton Ridenhour <ridenhour@gmail.com>
2023-05-09 11:20:55 -0400

    Neotree toggle

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -1,5 +1,7 @@
-require('neo-tree').setup({
-  filesystem = {
-    hijack_netrw_behavior = "open_default",
-  },
-});
+require("neo-tree").setup({
+	filesystem = {
+		hijack_netrw_behavior = "open_default",
+	},
+})
+
+vim.keymap.set("n", "<leader>\\", "<Cmd>Neotree toggle<CR>")
